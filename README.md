f6d9a74b0808e8084459db247f6301ce1f1aa080
Raul Gallo Dagir <rauldagir@hotmail.com>
2019-12-01 14:51:37 -0800

    Model works and flags make sense. Accuracy still shit. Deleting old unnecessary files.

--- README.md
+++ README.md
@@ -5,3 +5,16 @@ MBAs are very quiet
 
 Sites for scikit-image:
 - Face detector: https://scikit-image.org/docs/stable/auto_examples/applications/plot_face_detection.html#sphx-glr-auto-examples-applications-plot-face-detection-py
+
+## Instructions:
+### Scraping the dataset:
+1. Create a csv file with only one column "hashtag", and list all hashtags you want to scrape below it.
+2. Call python3 scraper.py csvfilename
+3. The scraped dataset will be saved as csvfilenameFULL.csv
+### Downloading images/training model/making predictions:
+- The raul-imgclassifier.py program takes multiple possible arguments
+1. Call python3 raul-imgclassifier.py -d datasetname to download images from that dataset, preprocess everything, train the model and make a prediction given the dev set.
+2. Call python3 raul-imgclassifier.py -f imgfilename resultsfilename where both files are ".npy" files. Instead of downloading the images from the dataset, it will load the data from those files, then  train the model and make a prediction given the test set.
+3. Call python3 raul-imgclassifier.py -f imgfilename resultsfilename -m modelfilename if you want to run the prediction given a previously trained model, defined by the modelfilename argument.
+
+Have fun!
--- main/raul-imgclassifier.py
+++ main/raul-imgclassifier.py
@@ -4,14 +4,14 @@ import imageprocess as imageProcess
 import csv
 from skimage.transform import rescale, resize, downscale_local_mean
 from keras.models import Sequential, load_model
-from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
+from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Dropout
 import matplotlib.pyplot as plt
 import math
-from sklearn.metrics import log_loss
+from keras import regularizers
+import random
 
 TARGET_X = 270
 TARGET_Y = 270
-
 def downloadImages(dataset):
     print('Start reading features')
     with open(dataset) as f:
@@ -21,6 +21,8 @@ def downloadImages(dataset):
         totalImgs = 0
         correctShape = 0
         for row in csv.DictReader(f):
+            if (float(row["likeRatio"]) > 1.):
+                continue
             print(totalImgs)
             totalImgs += 1
             try:
@@ -38,9 +40,15 @@ def downloadImages(dataset):
                 continue
             allImgs.append(image_rescaled)
             allResults.append(float(row["likeRatio"]))
+    print("shuffling and splitting dataset...")
+    zipped = list(zip(allImgs, allResults))
+    random.shuffle(zipped)
+    allImgs, allResults = list(zip(*zipped))
     print("not processed: " + str(notProcessed/totalImgs))
-    np.save(f"allImgs_{dataset[9:-4]}.npy", allImgs)
-    np.save(f"allResults_{dataset[9:-4]}.npy", allResults)
+    slashIndex = dataset.find("/")
+    slashIndex += 1
+    np.save(f"allImgs_{dataset[slashIndex:-4]}.npy", allImgs)
+    np.save(f"allResults_{dataset[slashIndex:-4]}.npy", allResults)
     return allImgs, allResults
 
 def loadFromFile(imgsfile, resultsfile):
@@ -66,29 +74,32 @@ def preprocess(allImgs, allResults):
     y_test_one_hot = oneHotEncoding(y_test)
     return x_train, x_dev, x_test, y_train, y_dev, y_test, y_train_one_hot, y_test_one_hot
 
-def trainModel(x_train, y_train, x_test, y_test):
+def trainModel(modelfilename, x_train, y_train, x_test, y_test):
     #create model
     model = Sequential()
 
     #add model layers
     model.add(Conv2D(16, kernel_size=3, activation='relu', input_shape=(TARGET_X, TARGET_Y,3)))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+    # model.add(Dropout(0.3))
     model.add(Conv2D(32, kernel_size=3, activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+    # model.add(Dropout(0.3))
     model.add(Conv2D(64, kernel_size=3, activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+    # model.add(Dropout(0.3))
     model.add(Conv2D(128, kernel_size=3, activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(256, kernel_size=3, activation='relu'))
     model.add(Flatten())
     model.add(Dense(100, activation='softmax'))
+                # kernel_regularizer=regularizers.l2(0.01),
+                # activity_regularizer=regularizers.l1(0.01)))
 
     #compile model using accuracy to measure model performance
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
     #train the model
     model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=15)
-    model.save(MODEL_PATH)
+    model.save(f"models/{modelfilename}.h5")
     return model
 
 def predict(model, x_dev, y_dev):
@@ -99,36 +110,35 @@ def predict(model, x_dev, y_dev):
 
     predictions = model.predict(x_dev)
 
-    scores = []
-    for prediction in predictions:
-        scores.append(np.dot(prediction, [i for i in range(1,11)]))
-    scores = np.array(scores)
-    predictedImageIndex = np.argmax(scores)
-    print(f"scores: {scores}")
-    print(f"predictedImageIndex: {predictedImageIndex}")
-    plt.figure()
-    plt.imshow(x_dev[dotProdIndex])
-    plt.show()
-    
-    # maxBucket = 0 
-    # maxProbsForMaxBucket = 0.0
-    # predictedImageIndex = 0
-    # for i in range(len(predictions)):
-    #     prediction = predictions[i]
-    #     maxResultIndex = np.argmax(prediction)
-    #     maxResult = prediction[maxResultIndex]
-    #     if maxBucket <= maxResultIndex:
-    #         maxBucket = maxResultIndex
-    #         maxProbsForMaxBucket = maxResult
-    #         predictedImageIndex = i
-    #     elif maxBucket == maxResultIndex:
-    #         if maxProbsForMaxBucket < maxResult:
-    #             maxProbsForMaxBucket = maxResult
-    #             predictedImageIndex = i
-
+    # scores = []
+    # for prediction in predictions:
+    #     scores.append(np.dot(prediction, [i for i in range(1,101)]))
+    # scores = np.array(scores)
+    # predictedImageIndex = np.argmax(scores)
+    # print(f"scores: {scores}")
     # print(f"predictedImageIndex: {predictedImageIndex}")
     # plt.figure()
     # plt.imshow(x_dev[predictedImageIndex])
+    
+     # WRONG - USE PHIL'S VERSION ABOVE
+    maxBucket = 0 
+    maxProbsForMaxBucket = 0.0
+    predictedImageIndex = 0
+    for i in range(len(predictions)):
+        prediction = predictions[i]
+        maxResultIndex = np.argmax(prediction)
+        maxResult = prediction[maxResultIndex]
+        if maxBucket <= maxResultIndex:
+            maxBucket = maxResultIndex
+            maxProbsForMaxBucket = maxResult
+            predictedImageIndex = i
+        elif maxBucket == maxResultIndex:
+            if maxProbsForMaxBucket < maxResult:
+                maxProbsForMaxBucket = maxResult
+                predictedImageIndex = i
+    print(f"predictedImageIndex: {predictedImageIndex}")
+    plt.figure()
+    plt.imshow(x_dev[predictedImageIndex])
 
 
     plt.figure()
@@ -137,6 +147,7 @@ def predict(model, x_dev, y_dev):
 
 
 def splitDataset(allImgs, allResults):
+
     limitTrain = 6*int(len(allImgs)/10)
     x_train = np.array(allImgs[:limitTrain])
     y_train = np.array(allResults[:limitTrain])
@@ -149,6 +160,20 @@ def splitDataset(allImgs, allResults):
     y_test = np.array(prevy_test[:limitTest])
     return (x_train, x_dev, x_test), (y_train, y_dev, y_test)
 
+def extractDatasetNameNPY(datafilename):
+    extracted = datafilename[:-4] # removing .csv
+    indexUnderscore = datafilename.find("_")
+    indexUnderscore += 1
+    extracted = extracted[indexUnderscore:]
+    return extracted
+
+def extractDatasetNameCSV(datafilename):
+    extracted = datafilename[:-4] # removing .csv
+    slashIndex = datafilename.find("/")
+    slashIndex += 1
+    extracted = extracted[slashIndex:]
+    return extracted
+
 if __name__ == "__main__":
     if (len(sys.argv) < 2):
         print("Don't forget the flag!")
@@ -156,7 +181,7 @@ if __name__ == "__main__":
     if (sys.argv[1] == "-d"):
         allImgs, allResults = downloadImages(sys.argv[2])
         x_train, x_dev, x_test, y_train, y_dev, y_test, y_train_one_hot, y_test_one_hot = preprocess(allImgs, allResults)
-        model = trainModel(x_train, y_train_one_hot, x_test, y_test_one_hot)
+        model = trainModel(extractDatasetNameCSV(sys.argv[2]), x_train, y_train_one_hot, x_test, y_test_one_hot)
     elif (sys.argv[1] == "-f"):
         allImgs, allResults = loadFromFile(sys.argv[2], sys.argv[3])
         x_train, x_dev, x_test, y_train, y_dev, y_test, y_train_one_hot, y_test_one_hot = preprocess(allImgs, allResults)        
@@ -164,7 +189,7 @@ if __name__ == "__main__":
             if(sys.argv[4] == "-m"):
                 model = load_model(sys.argv[5])
         except:
-            model = trainModel(x_train, y_train_one_hot, x_test, y_test_one_hot)    
+            model = trainModel(extractDatasetNameNPY(sys.argv[2]), x_train, y_train_one_hot, x_test, y_test_one_hot)    
     else:
         print("Invalid flag, mate!")
         sys.exit(0)
