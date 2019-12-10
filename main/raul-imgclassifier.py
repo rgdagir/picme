c36224c34ac398aba48b9d96367a4c7c8f73f013
Raul Gallo Dagir <rauldagir@hotmail.com>
2019-12-09 16:36:38 -0800

    Concatenated  models

--- main/raul-imgclassifier.py
+++ main/raul-imgclassifier.py
@@ -3,10 +3,9 @@ import numpy as np
 import imageprocess as imageProcess
 import csv
 from skimage.transform import rescale, resize, downscale_local_mean
-from keras.models import Sequential, load_model
+from keras.models import Sequential, load_model, Model
 from keras.callbacks import EarlyStopping
 from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Dropout, Conv1D, MaxPooling1D, AveragePooling2D, BatchNormalization, concatenate
-
 import matplotlib.pyplot as plt
 import math
 from keras import regularizers
@@ -14,8 +13,8 @@ import random
 from datetime import datetime 
 import textdistance
 from keras.utils.vis_utils import plot_model
-from skimage.color import rgb2gray
 from keras.optimizers import Adam
+from utils import *
 
 TARGET_X = 135
 TARGET_Y = 135
@@ -63,25 +62,6 @@ def downloadImages(dataset):
     np.save(f"allImgs_{dataset[slashIndex:-4]}.npy", allImgs)
     np.save(f"allResults_{dataset[slashIndex:-4]}.npy", allResults)
     return allImgs, allResults
-
-def loadFromFile(files):
-    out = []
-    for f in files:
-        try:
-            out.append(np.load(f))
-        except Exception as e:
-            print(e)
-    return out
-    
-def oneHotEncoding(arr):
-    oneHots = []
-    for y in arr:
-        index = int(y*100)%100
-        oneHot = []
-        for i in range(100):
-            oneHot.append(0 if i != index else 1)
-        oneHots.append(oneHot)
-    return np.array(oneHots)
 
 ############################################################
 # Feature extraction
@@ -183,24 +163,12 @@ def extractFeaturesFromDataset(filename):
         plt.title('image shape distribution')
         plt.ylabel('width')
         plt.xlabel('height')
-        plt.savefig(f"datasets/{dataset[slashIndex:-4]}_distribution.png")
+        plt.savefig(f"datasets/{filename[slashIndex:-4]}_distribution.png")
         np.save(f"allImgs_{filename[slashIndex:-4]}.npy", allImgs)
         np.save(f"allResults_{filename[slashIndex:-4]}.npy", allResults)
         np.save(f"featureVectors_{filename[slashIndex:-4]}.npy", featureVectors)
         return allImgs, featureVectors, allResults
 
-
-
-def splitAndPrep(allImgs, allResults):
-    # print("shuffling and splitting dataset...")
-    # zipped = list(zip(allImgs, allResults))
-    # random.shuffle(zipped)
-    # allImgs, allResults = list(zip(*zipped))
-    (x_train, x_dev, x_test), (y_train, y_dev, y_test) = splitDataset(allImgs, allResults)
-
-    y_train_one_hot = oneHotEncoding(y_train)
-    y_test_one_hot = oneHotEncoding(y_test)
-    return x_train, x_dev, x_test, y_train, y_dev, y_test, y_train_one_hot, y_test_one_hot
 
 
 def trainModel(modelfilename, x_train, y_train, x_test, y_test, concat=False):
@@ -225,28 +193,28 @@ def trainModel(modelfilename, x_train, y_train, x_test, y_test, concat=False):
 
     model.add(Conv2D(16, kernel_size=(3,3), input_shape=(TARGET_X, TARGET_Y, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(BatchNormalization())
+    # model.add(BatchNormalization())
     model.add(Conv2D(32, kernel_size=(3,3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(BatchNormalization())
+    # model.add(BatchNormalization())
     model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(BatchNormalization())
+    # model.add(BatchNormalization())
     model.add(Conv2D(128, kernel_size=(3,3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(BatchNormalization())
+    # model.add(BatchNormalization())
     model.add(Flatten())
-    model.add(Dense(2000, activation='relu'))
+    model.add(Dense(1250, activation='relu'))
     model.add(Dropout(.5))
-    model.add(BatchNormalization())
-    model.add(Dense(1000, activation='relu'))
+    # model.add(BatchNormalization())
+    model.add(Dense(250, activation='relu'))
     model.add(Dropout(.25))
-    model.add(BatchNormalization())
-    model.add(Dense(500, activation='relu'))
-    model.add(BatchNormalization())
+    # model.add(BatchNormalization())
+    model.add(Dense(25, activation='relu'))
+    # model.add(BatchNormalization())
 
     if concat:
-        model.add(Dense(100, activation = 'relu'))
+        model.add(Dense(4, activation = 'relu'))
         return model
     else:
         model.add(Dense(100, activation = 'softmax'))
@@ -297,14 +265,15 @@ def trainMdModel(modelfilename, x_train, y_train, x_test, y_test, concat=False):
     model = Sequential()
 
     model = Sequential()
-    model.add(Dense(2000, input_dim=46, activation="relu"))
-    model.add(Dense(1000, activation="relu"))
-    model.add(Dense(500, activation="relu"))
-    model.add(Dense(250, activation="relu"))
-    model.add(Dense(100, activation="relu"))
-
+    model.add(Dense(64, input_dim=10, activation="relu"))
+    model.add(Dense(32, activation="relu"))
+    model.add(Dense(16, activation="relu"))
+    model.add(Dense(8, activation="relu"))
     if concat:
+        model.add(Dense(4, activation = 'relu'))
         return model
+    else:
+        model.add(Dense(100, activation = 'softmax'))
 
     es = EarlyStopping(monitor='val_loss', mode='min', verbose=1)
     #compile model using accuracy to measure model performance
@@ -315,7 +284,7 @@ def trainMdModel(modelfilename, x_train, y_train, x_test, y_test, concat=False):
 
     print(model.summary())
 
-    plot_model(model, to_file=f"models/{modelfilename}_plot.png", show_shapes=True, show_layer_names=True)
+    plot_model(model, to_file=f"models/{modelfilename}_metadata_plot.png", show_shapes=True, show_layer_names=True)
 
     plt.figure()
     plt.plot(history.history['accuracy'])
@@ -324,7 +293,7 @@ def trainMdModel(modelfilename, x_train, y_train, x_test, y_test, concat=False):
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
     plt.legend(['train', 'validation'], loc='upper left')
-    plt.savefig(f"models/{modelfilename}_accuracy.png")
+    plt.savefig(f"models/{modelfilename}_metadata_accuracy.png")
 
     plt.figure()
     plt.plot(history.history['cosine_proximity'])
@@ -333,7 +302,7 @@ def trainMdModel(modelfilename, x_train, y_train, x_test, y_test, concat=False):
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
     plt.legend(['train', 'validation'], loc='upper left')
-    plt.savefig(f"models/{modelfilename}_cosineproximity.png")
+    plt.savefig(f"models/{modelfilename}_metadata_cosineproximity.png")
 
     plt.figure()
     plt.plot(history.history['loss'])
@@ -342,49 +311,9 @@ def trainMdModel(modelfilename, x_train, y_train, x_test, y_test, concat=False):
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(['train', 'validation'], loc='upper left')
-    plt.savefig(f"models/{modelfilename}_loss.png")
+    plt.savefig(f"models/{modelfilename}_metadata_loss.png")
 
     model.save(f"models/{modelfilename}.h5")
-    return model
-
-def trainModelFeatureVec(modelfilename, x_train, y_train, x_test, y_test):
-    #create model
-    model1 = Sequential()
-    model1.add(Dense(64, input_dim=46, activation="relu"))
-    model1.add(Dense(32, activation="relu"))
-    model1.add(Dense(16, activation="relu"))
-    model1.add(Dense(8, activation="relu"))
-    model1.add(Dense(4, activation="relu"))
-
-    #compile model using accuracy to measure model performance
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-    #train the model
-    history = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=15)
-
-    print(model.summary())
-
-    plot_model(model, to_file=f"models/featureVec_{modelfilename}_plot.png", show_shapes=True, show_layer_names=True)
-
-    plt.figure()
-    plt.plot(history.history['accuracy'])
-    plt.plot(history.history['val_accuracy'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'validation'], loc='upper left')
-    plt.savefig(f"models/featureVec_{modelfilename}_accuracy.png")
-
-    plt.figure()
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'validation'], loc='upper left')
-    plt.savefig(f"models/featureVec_{modelfilename}_loss.png")
-
-    model.save(f"models/featureVec_{modelfilename}.h5")
     return model
 
 def test_model(model, x, y):
@@ -455,57 +384,10 @@ def predict(model, x_dev, y_dev):
     # plt.figure()
     # plt.imshow(x_dev[predictedImageIndex])
 
-
     plt.figure()
     plt.imshow(x_dev[actualBestImageIndex])
     plt.show()
 
-
-def splitDataset(allImgs, allResults):
-
-    limitTrain = 6*int(len(allImgs)/10)
-    x_train = np.array(allImgs[:limitTrain])
-    y_train = np.array(allResults[:limitTrain])
-    prevx_test = np.array(allImgs[limitTrain:])
-    prevy_test = np.array(allResults[limitTrain:])
-    limitTest = int(len(prevx_test)/2)
-    x_dev = np.array(prevx_test[limitTest:])
-    y_dev = np.array(prevy_test[:limitTest])
-    x_test = np.array(prevx_test[:limitTest])
-    y_test = np.array(prevy_test[:limitTest])
-    # x_train = x_train.reshape(len(x_train),TARGET_X, TARGET_Y,1)
-    # x_test = x_test.reshape(len(x_test),TARGET_X, TARGET_Y,1)
-    # x_dev = x_test.reshape(len(x_dev),TARGET_X, TARGET_Y,1)
-    return (x_train, x_dev, x_test), (y_train, y_dev, y_test)
-
-def splitList(lst, sz):
-    lol = lambda lst, sz: [lst[i:i+sz] for i in range(0, len(lst), sz)]
-    return lol(lst, sz)
-
-def extractDatasetNameNPY(datafilename):
-    extracted = datafilename[:-4] # removing .csv
-    indexUnderscore = datafilename.find("_")
-    indexUnderscore += 1
-    extracted = extracted[indexUnderscore:]
-    return extracted
-
-def extractDatasetNameCSV(datafilename):
-    extracted = datafilename[:-4] # removing .csv
-    slashIndex = datafilename.find("/")
-    slashIndex += 1
-    extracted = extracted[slashIndex:]
-    return extracted
-
-def grayscaleResize(allImgs):
-    print("greyscaling and resizing...")
-    graySmallImgs = []
-    for img in allImgs:
-        grayscale = rgb2gray(img)
-        image_rescaled = resize(grayscale, (TARGET_X, TARGET_Y),anti_aliasing=False)
-        graySmallImgs.append(image_rescaled)
-    rgb_batch = np.repeat(graySmallImgs, 3)
-    rgb_batch = rgb_batch.reshape(len(graySmallImgs),TARGET_X, TARGET_Y,3)
-    return np.array(rgb_batch)
 
 def concatenatedModelMain():
     if (len(sys.argv) < 2):
@@ -518,12 +400,12 @@ def concatenatedModelMain():
         # assert(imgY_train, mdY_train) # raul-imgclassifier.py:537: SyntaxWarning: assertion is always true, perhaps remove parentheses?
         # imgX_train = imgX_train.reshape(imgX_train.shape[0], TARGET_X, TARGET_Y, 3)
         # imgX_test = imgX_test.reshape(imgX_test.shape[0], TARGET_X, TARGET_Y, 3)
-        imageModel = trainModel(extractDatasetNameCSV(sys.argv[2]), imgX_train, imgY_train_one_hot, imgX_test, imgX_test_one_hot, True)
+        imageModel = trainModel(extractDatasetNameCSV(sys.argv[2]), imgX_train, imgY_train_one_hot, imgX_test, imgY_test_one_hot, True)
         mdModel = trainMdModel(extractDatasetNameCSV(sys.argv[2]), mdX_train, mdY_train_one_hot, mdX_test, mdY_test_one_hot, True)
 
         combinedInput = concatenate([mdModel.output, imageModel.output])
-        x = Dense(250, activation="relu")(combinedInput)
-        x = Dense(100, activation="softmax")(x)
+        x = Dense(4, activation="relu")(combinedInput)
+        x = Dense(1, activation="softmax")(x)
         concatModel = Model(inputs=[mdModel.input, imageModel.input], outputs=x)
         concatModel.compile(optimizer=Adam(lr=1e-4, decay=1e-4 / 200), loss='categorical_crossentropy', metrics=['accuracy', 'cosine_proximity'])
         concatModel.fit([mdX_train, imgX_train], mdY_train,validation_data=([mdX_test, imgX_test], mdY_test), epochs=100, batch_size=8)
@@ -532,19 +414,20 @@ def concatenatedModelMain():
     elif (sys.argv[1] == "-f"):
         allImgs, allResults, featureVectors = loadFromFile([sys.argv[2], sys.argv[3], sys.argv[4]])
         # allImgs = grayscaleResize(allImgs)
-        x_train, x_dev, x_test, y_train, y_dev, y_test, y_train_one_hot, y_test_one_hot = splitAndPrep(allImgs, allResults)
+        imgX_train, imgX_dev, imgX_test, imgY_train, imgY_dev, imgY_test, imgY_train_one_hot, imgY_test_one_hot = splitAndPrep(allImgs, allResults)
         mdX_train, mdX_dev, mdX_test, mdY_train, mdY_dev, mdY_test, mdY_train_one_hot, mdY_test_one_hot = splitAndPrep(featureVectors, allResults)
         # assert(imgY_train, mdY_train) #raul-imgclassifier.py:537: SyntaxWarning: assertion is always true, perhaps remove parentheses?
         # imgX_train = imgX_train.reshape(imgX_train.shape[0], TARGET_X, TARGET_Y, 3)
         # imgX_test = imgX_test.reshape(imgX_test.shape[0], TARGET_X, TARGET_Y, 3)
-        imageModel = trainModel(extractDatasetNameCSV(sys.argv[2]), imgX_train, imgY_train_one_hot, imgX_test, imgX_test_one_hot, True)
+        imageModel = trainModel(extractDatasetNameCSV(sys.argv[2]), imgX_train, imgY_train_one_hot, imgX_test, imgY_test_one_hot, True)
         mdModel = trainMdModel(extractDatasetNameCSV(sys.argv[2]), mdX_train, mdY_train_one_hot, mdX_test, mdY_test_one_hot, True)
 
         combinedInput = concatenate([mdModel.output, imageModel.output])
-        x = Dense(250, activation="relu")(combinedInput)
-        x = Dense(100, activation="softmax")(x)
+        x = Dense(4, activation="relu")(combinedInput)
+        x = Dense(1, activation="softmax")(x)
         concatModel = Model(inputs=[mdModel.input, imageModel.input], outputs=x)
-        concatModel.compile(optimizer=Adam(lr=1e-4, decay=1e-4 / 200), loss='categorical_crossentropy', metrics=['accuracy', 'cosine_proximity'])
+        plot_model(concatModel, to_file=f"models/concatModel_plot.png", show_shapes=True, show_layer_names=True)
+        concatModel.compile(optimizer=Adam(lr=1e-4, decay=1e-4 / 200), loss='mean_squared_error', metrics=['accuracy', 'cosine_proximity'])
         concatModel.fit([mdX_train, imgX_train], mdY_train,validation_data=([mdX_test, imgX_test], mdY_test), epochs=100, batch_size=8)
 
         # try:
@@ -560,6 +443,49 @@ def concatenatedModelMain():
     # split_result_test = splitList(y_test, 10)   
     # for i in range(len(split_result_test)):
     #     test_model(model, split_test_set[i], split_result_test[i])
+
+##########################
+####    DUMPSTER    ######
+##########################
+def trainModelFeatureVec(modelfilename, x_train, y_train, x_test, y_test):
+    #create model
+    model = Sequential()
+    model.add(Dense(64, input_dim=46, activation="relu"))
+    model.add(Dense(32, activation="relu"))
+    model.add(Dense(16, activation="relu"))
+    model.add(Dense(8, activation="relu"))
+    model.add(Dense(4, activation="relu"))
+
+    #compile model using accuracy to measure model performance
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    #train the model
+    history = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=15)
+
+    print(model.summary())
+
+    plot_model(model, to_file=f"models/featureVec_{modelfilename}_plot.png", show_shapes=True, show_layer_names=True)
+
+    plt.figure()
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.savefig(f"models/featureVec_{modelfilename}_accuracy.png")
+
+    plt.figure()
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.savefig(f"models/featureVec_{modelfilename}_loss.png")
+
+    model.save(f"models/featureVec_{modelfilename}.h5")
+    return model
 
 def oldmain():
     if (len(sys.argv) < 2):
